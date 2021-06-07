13d4610ff536205a1822fe38688fd3f5726d9f19
schaves <schaves@Sebastian>
2021-06-07 09:46:18 -0600

    semana2y3

---
+++ src/claseSemana2.java
@@ -0,0 +1,37 @@
+
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author schaves
+ */
+public class claseSemana2 { 
+    public static void main(String[] args){
+        Scanner escaner = new Scanner(System.in);
+          
+          System.out.println("Universidad de Costa Rica");
+          System.out.println("sebastian chaves");
+          System.out.println();
+          System.out.println("Por favor, digite un numero");
+          
+          int num2 =   escaner.nextInt();
+          System.out.println("Por favor, digite otro numero");
+          int box1 = escaner.nextInt();
+          System.out.println("resultado");
+          String nombre = ("Sebastian Chaves");
+           System.out.println(nombre=""+box1);
+       }
+                     
+    
+          
+    }           
+            
+
+    
+
---
+++ src/tareaSemana2.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author schaves
+ */
+public class tareaSemana2 {
+    public static void main(String[] args){
+        int box1;
+	int num2;
+	int resultado;
+	box1 = 5;
+	num2 = 30;
+
+	resultado = box1 + num2;
+		
+	System.out.println("resultado");
+	float salario;
+	salario = 1020239.43f;
+	double resultadoCientifico;
+	resultadoCientifico = 1234.12312342;
+		
+	boolean siSalimosMediaNoche;
+	siSalimosMediaNoche = false;
+		
+	String nombre;
+	nombre = "Sebastian Chaves";
+	char tipoSangre;
+         tipoSangre = 'o';
+         System.out.println("resultado");
+    }
+    
+}
