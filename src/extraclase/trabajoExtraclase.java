392fce11ddfc43fbab6e66196588ea72255db4c2
schaves <schaves@Sebastian>
2021-06-17 10:40:46 -0600

    semana2y3

--- src/extraclase/trabajoExtraclase.java
+++ src/extraclase/trabajoExtraclase.java
@@ -1,7 +1,7 @@
 package extraclase;
 
 
-import <any?>;
+
 import java.util.Scanner;
 
 /*
@@ -16,36 +16,43 @@ import java.util.Scanner;
  */
 public class trabajoExtraclase {
  
-    public static double calcularImpuesto(int numDependientes, double salario);
-  {
-   
-    double monto=0;
-    if (salario <= 200000);
-    monto = 0;
-    
-    else if (salario <= 400000);
-    monto = 0.10;
-    
-    else if (salario <= 600000);
-    monto = 0.15;
-    
-    else if (salario <= 800000);
-    monto = 0.20;
-    else if (salario > 800000);
-    monto = 0.30
+    public static double calcularImpuesto(int numDependientes, double salario){
   
-  double monto2 = 0;
-   if (numDependientes == 1);
-   monto2 *1;
-   else if (numDependientes == 2);
-   monto2 *0.90;
-   else if (numdDependientes == 3);
-   monto2 *0.70;
-   else if (numDependientes ==4);
-   monto2 * 0.60;
-   else if(numDependientes > 5);
-   monto2 = salario*0.40;
-   double impuesto == monto2*monto;
+   
+    double monto=0.0;
+    if (salario <= 200000){
+    monto = 0.0;}
+    
+    else if (salario <= 400000){
+    monto = 0.10;}
+    
+    else if (salario <= 600000){
+    monto = 0.15;}
+    
+    else if (salario <= 800000){
+    monto = 0.20;}
+    else if (salario > 800000);{
+    monto = 0.30;}
+  
+ 
+ 
+    double monto2 = 0;
+   
+  if (numDependientes==1)
+   monto2 =salario*1;
+  
+  else if (numDependientes==2){
+   monto2= salario*0.90;}
+  
+  else if (numDependientes==3){
+   monto2= salario*0.70;}
+  
+  else if (numDependientes==4){
+   monto2=salario * 0.60;}
+  
+  else if(numDependientes > 5){
+   monto2 = salario*0.40;}
+   double impuesto = monto2*monto;
    return impuesto; 
   
            
@@ -54,14 +61,14 @@ public class trabajoExtraclase {
   public static void main(String[] args);{
   scaner= new Scanner(System.in);
  System.out.println("Porfavor Introduzca su salario");
- double salario= entrada.nextDouble();
+ double salario = entrada.nextDouble();
  
 System.out.println("Introduzca la dependiente");
 int numDependiente = entrada.nextInt();
-double pagar = calcularImpuesto(numDependientes.salario);
+double pagar = calcularImpuesto(numDependientes,salario);
 
 System.out.println("El monto a pagar es de: ");
 
-       
+   }     
   
-} }
+} 
