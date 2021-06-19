2d8be659b75f268a3b68808e62622a3d5492b259
schaves <schaves@Sebastian>
2021-06-18 18:39:11 -0600

    semana2y3

---
+++ src/Quiz1.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author schaves
+ */
+public class Quiz1 {
+    public static void double monto=0 (Strig []args);    
+double ingrediente1=1000;
+double ingrediente2=800;
+double ingrediente3=1200;
+int mini;
+int grande;
+
+ {
+if 	(ingrediente1= mini);
+ monto=(ingrediente1*0.10)+ingrediente1}
+System.out.println("su monto es de 1100");
+else if (ingrediente1= grande){
+monto=(ingrediente1*0.40)+ingrediente1}
+System.out.println("su monto es de 1400");
+ }	
+{
+if 	(ingrediente2= mini);
+ monto=(ingrediente2*0.10)+ingrediente2}
+System.out.println("su monto es de 880");
+else if (ingrediente1= grande){
+monto=(ingrediente2*0.40)+ingrediente2}
+System.out.println("su monto es de 1120");
+
+ }	
+ {
+if 	(ingrediente3= mini);
+ monto=(ingrediente3*0.10)+ingrediente3}
+System.out.println("su monto es de 1320");
+else if (ingrediente3= grande){
+monto=(ingrediente3*0.40)+ingrediente3}
+System.out.println("su monto es de 1680");
+
+ }	
+ public static void main(String []args);
+ escaner= new scanner(System.in);
+ System.out.println("Digite 1 si o 0 no, en los ingredientes");
+    
+}
